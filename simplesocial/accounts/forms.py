caf02eafe5323a2c77087c3875c807b0229b7f5f
Aleksander Jaworski <aleck156@gmail.com>
2020-05-25 11:04:23 +0200

    add get_user_model and UserCreationForms imports

---
+++ simplesocial/accounts/forms.py
@@ -0,0 +1,7 @@
+# returns user model that is currently active in use
+from django.contrib.auth import get_user_model
+
+# user creatin form is already built-in
+# handles everything that's needed to create user
+# auth.models.User is created properly with this thing
+from django.contrib.auth.forms import UserCreationForm
