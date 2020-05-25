e84c16a51ebcea1e69c1582e3b67bbc5583e4369
Aleksander Jaworski <aleck156@gmail.com>
2020-05-25 11:27:52 +0200

    define __init__ method

--- simplesocial/accounts/forms.py
+++ simplesocial/accounts/forms.py
@@ -12,5 +12,12 @@ class UserCreateForm(UserCreationForm):
         # full list of available fields:
         # https://docs.djangoproject.com/en/3.0/ref/contrib/auth/
         # under User model description
+        # https://docs.djangoproject.com/en/1.8/_modules/django/contrib/auth/forms/
         fields = ('username', 'email', 'password1', 'password2')
         model = get_user_model()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # customization labels
+        self.fields['username'] = 'Display Name'
+        self.fields['email'] = 'Email Address'
