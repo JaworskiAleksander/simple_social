012c7de2db09c5c61b6def9f0f1ca7d03bc2df3e
Aleksander Jaworski <aleck156@gmail.com>
2020-05-25 11:00:23 +0200

    create SignUp class, extends from CreateView, add reverse_lazy import

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,11 @@
+# django imports
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+# project/app imports
+from . import forms
 
 # Create your views here.
+class SignUp(CreateView):
+    pass
