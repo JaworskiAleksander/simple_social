453fe04d54db255874edf3792fbdbe8e5209ddd3
Aleksander Jaworski <aleck156@gmail.com>
2020-05-25 11:31:53 +0200

    define form_class, success_url and template_name attributes

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -8,4 +8,10 @@ from . import forms
 
 # Create your views here.
 class SignUp(CreateView):
-    pass
+    # define which form will be used in this view
+    # no parenthesis, I don't want to instantiatize this yet
+    form_class = forms.UserCreateForm
+    # once someone has successfuly singed up, send them to page that's under 'login' alias name
+    # reverse_lazy is executed AFTER submit button is pressed
+    success_url = reverse_lazy('login')
+    template_name = 'acounts/signup.html'
