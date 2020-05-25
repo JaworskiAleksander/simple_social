2dd9ea68aa3401e6296e855e6ccc65f42e7d28d8
Aleksander Jaworski <aleck156@gmail.com>
2020-05-25 11:23:44 +0200

    set fields and model attributes in Meta class

--- simplesocial/accounts/forms.py
+++ simplesocial/accounts/forms.py
@@ -7,4 +7,10 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
-    pass
+
+    class Meta:
+        # full list of available fields:
+        # https://docs.djangoproject.com/en/3.0/ref/contrib/auth/
+        # under User model description
+        fields = ('username', 'email', 'password1', 'password2')
+        model = get_user_model()
