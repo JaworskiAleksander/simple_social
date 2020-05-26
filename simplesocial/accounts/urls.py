78919df0b342c417daa188e10e3bcc4338f37df4
Aleksander Jaworski <aleck156@gmail.com>
2020-05-26 09:20:33 +0200

    add comment on auth_views, import views from accounts

--- simplesocial/accounts/urls.py
+++ simplesocial/accounts/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+# to avoid mixing up views from contrib.auth and app views
+# I'm importing contrib.auth as auth_views
 from django.contrib.auth import views as auth_views
 
+from . import views
