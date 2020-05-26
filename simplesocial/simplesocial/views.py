c262371005e677732ab3afb6b7ebec4111f44a35
Aleksander Jaworski <aleck156@gmail.com>
2020-05-26 11:39:35 +0200

    set up TestPage class view

--- simplesocial/simplesocial/views.py
+++ simplesocial/simplesocial/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 # create a simple view to handle homepage
 class HomePage(TemplateView):
     template_name = 'index.html'
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
