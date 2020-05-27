64437e0441f07569aad4db91d777d2af9e85fad4
Aleksander Jaworski <aleck156@gmail.com>
2020-05-27 08:51:02 +0200

    import slugify and misaka module

--- simplesocial/groups/models.py
+++ simplesocial/groups/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+# use slugify to remove any non-alphanumeric characters
+# lowercase and add dashes instead of whitespaced
+from django.utils.text import slugify
+
+import misaka
 
 # Create your models here.
