89a1195500fd9a76fece3bba565222d36267e397
Aleksander Jaworski <aleck156@gmail.com>
2020-05-27 08:53:01 +0200

    import get_user_model from contrib.auth

--- simplesocial/groups/models.py
+++ simplesocial/groups/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # lowercase and add dashes instead of whitespaced
 from django.utils.text import slugify
 
+# markdown parsing library
 import misaka
 
+# import user model that is actively used in this project
+from django.contrib.auth import get_user_model
 # Create your models here.
