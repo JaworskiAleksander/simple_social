08cfbc51edcc85091bbe0f457e863bbf87e1f55a
Aleksander Jaworski <aleck156@gmail.com>
2020-05-27 10:36:47 +0200

    add imports in groups.views.py

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+from django.urls import reverse
+from django.views.generic import *
+
 # Create your views here.
+
+
